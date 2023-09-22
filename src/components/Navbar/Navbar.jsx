0d681867b79afe44214f9075b517adc9a6b3f50b
Lucas Neno <lucasneno@gmail.com>
2023-09-22 14:03:28 +0100

    cleaned up the code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import { React, useState } from 'react'
-import { FaBars, FaTimes, FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa';
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../../images/logo1.png'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
